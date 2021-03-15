865b7f665b12abbd9d84e0f3bc541d3ae9d18a85
Lefebvre Lionel <lenoill@hotmail.com>
2021-03-15 18:47:24 +0100

    style change, add jszip

--- main.js
+++ main.js
@@ -17,17 +17,33 @@ document.querySelector("#show-content").addEventListener("click", function () {
   isClicked("#show-content");
 });
 let clicked2 = false;
+let newLinks;
 document.querySelector("#check").addEventListener("click", () => {
   const arrLinks = text.split("\n");
-  arrLinks.map((item) => {
+    newLinks = arrLinks.map((item) => {
     const idGif = item.split("-").reverse();
     const url = `https://i.giphy.com/media/${idGif[0]}/giphy.gif`;
     const gifImg = document.createElement("img");
+    gifImg.className = "gif"
     gifImg.src = url;
+    gifImg.style.height = "150px";
     document.querySelector("#gif-check").appendChild(gifImg);
   });
+
+ 
+
+
   isClicked("#check");
 });
+
+document.querySelector("#download").addEventListener("click", () => {
+  let a = document.createElement("a");
+  a.href = "data:application/octet-stream," + encodeURIComponent(newLinks);
+  a.download = "nordvpnaccount.zip";
+  a.click();
+});
+
+
 
 function isClicked(id) {
   document.querySelector(id).disabled = true;
