246ab5353d996c4b9724efb396050e2ac37dbe51
Lefebvre Lionel <lenoill@hotmail.com>
2021-03-15 18:29:41 +0100

    check if show content and check gifs were clicked

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let text;
-let clicked = false;
 document.querySelector("#show-content").addEventListener("click", function () {
   let file = document.querySelector("#file-input").files[0];
   let reader = new FileReader();
@@ -7,26 +6,29 @@ document.querySelector("#show-content").addEventListener("click", function () {
     text = e.target.result;
 
     const arrLinks = text.split("\n");
-    arrLinks.map((item)=> {
-    const divLink = document.createElement('div');
-    divLink.innerText = `${item}`;
-    document.querySelector("#file-contents").appendChild(divLink);
-  })
-    
+    arrLinks.map((item) => {
+      const divLink = document.createElement("div");
+      divLink.innerText = `${item}`;
+      document.querySelector("#file-contents").appendChild(divLink);
+    });
   });
   reader.readAsText(file);
 
-  clicked = !clicked;
-  clicked === true ? document.querySelector("#show-content").disabled = true:"";
+  isClicked("#show-content");
 });
-
+let clicked2 = false;
 document.querySelector("#check").addEventListener("click", () => {
   const arrLinks = text.split("\n");
-  arrLinks.map((item)=> {
+  arrLinks.map((item) => {
     const idGif = item.split("-").reverse();
     const url = `https://i.giphy.com/media/${idGif[0]}/giphy.gif`;
-    const gifImg = document.createElement('img');
+    const gifImg = document.createElement("img");
     gifImg.src = url;
-    document.querySelector("#gif-check").appendChild(gifImg)
-  })
+    document.querySelector("#gif-check").appendChild(gifImg);
+  });
+  isClicked("#check");
 });
+
+function isClicked(id) {
+  document.querySelector(id).disabled = true;
+}
