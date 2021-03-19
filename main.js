083d7d59ad69ada06fe34375b06afb77bf051fc1
Lefebvre Lionel <lenoill@hotmail.com>
2021-03-19 15:59:51 +0100

    production version

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-
 let text;
+let newLinks = new Array();
+let idGif = "";
 document.querySelector("#show-content").addEventListener("click", function () {
   let file = document.querySelector("#file-input").files[0];
   let reader = new FileReader();
@@ -8,58 +9,48 @@ document.querySelector("#show-content").addEventListener("click", function () {
 
     const arrLinks = text.split("\n");
     arrLinks.map((item) => {
-      const divLink = document.createElement("div");
-      divLink.innerText = `${item}`;
-      document.querySelector("#file-contents").appendChild(divLink);
+      item.includes("-")
+        ? (idGif = item.split("-").reverse())
+        : (idGif = item.split("/").reverse());
+
+      const url = `https://i.giphy.com/media/${idGif[0]}/giphy.gif`;
+      newLinks.push(url);
+      const gifImg = document.createElement("img");
+      gifImg.className = "gif";
+      gifImg.src = url;
+      gifImg.style.height = "150px";
+      document.querySelector("#gif-check").appendChild(gifImg);
     });
   });
   reader.readAsText(file);
 
   isClicked("#show-content");
 });
-let clicked2 = false;
-let newLinks = new Array();
-document.querySelector("#check").addEventListener("click", () => {
-  const arrLinks = text.split("\n");
-    arrLinks.map((item) => {
-      
-    const idGif = item.split("-").reverse();
-    const url = `https://i.giphy.com/media/${idGif[0]}/giphy.gif`;
-    newLinks.push(url);
-    const gifImg = document.createElement("img");
-    gifImg.className = "gif"
-    gifImg.src = url;
-    gifImg.style.height = "150px";
-    document.querySelector("#gif-check").appendChild(gifImg);
-  });
-
- 
-
-
-  isClicked("#check");
-});
 
 document.querySelector("#download").addEventListener("click", () => {
- 
-  newLinks.forEach((item, index)=> {
-(async () => {
-  let a = document.createElement('a');
-  let response = await fetch(item);
-  let file = await response.blob();
-  a.download = `giphy${index}`;
-  a.href = window.URL.createObjectURL(file);
-  a.dataset.downloadurl = ['application/octet-stream', a.download, a.href].join(':');
-  a.click();
-})();
-  })
+  newLinks.forEach((item, index) => {
+    (async () => {
+      let a = document.createElement("a");
+      let response = await fetch(item);
+      let file = await response.blob();
+
+      //give the names of the files
+      const inputName = document.getElementById("save-name").value;
+      inputName === ""
+        ? (a.download = `giphy${index}`)
+        : (a.download = `${inputName}${index}`);
+
+      a.href = window.URL.createObjectURL(file);
+      a.dataset.downloadurl = [
+        "application/octet-stream",
+        a.download,
+        a.href,
+      ].join(":");
+      a.click();
+    })();
+  });
 });
-
-
-
-
 
 function isClicked(id) {
   document.querySelector(id).disabled = true;
 }
-
-
